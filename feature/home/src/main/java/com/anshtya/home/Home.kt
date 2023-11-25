43b5c5d62710500fa451197ade4b6d61f0db9c1b
Ansh Tyagi <95761927+anshtya@users.noreply.github.com>
2023-11-25 18:56:54 +0530

    basic trending movies implemented

--- feature/home/src/main/java/com/anshtya/home/Home.kt
+++ feature/home/src/main/java/com/anshtya/home/Home.kt
@@ -1,18 +1,33 @@
 package com.anshtya.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
@@ -27,13 +42,15 @@ fun HomeRoute(
     val trendingMovies = homeViewModel.trendingMovies.collectAsLazyPagingItems()
 
     Home(
-        trendingMovies = trendingMovies
+        trendingMovies = trendingMovies,
+        onItemClick = { homeViewModel.setTrendingTimeWindow(it) }
     )
 }
 
 @Composable
 fun Home(
     trendingMovies: LazyPagingItems<StreamingItem>,
+    onItemClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Surface(
@@ -43,10 +60,21 @@ fun Home(
             modifier = Modifier.fillMaxSize()
         ) {
             item {
-                Text(
-                    text = "Trending",
-                    style = MaterialTheme.typography.headlineMedium
-                )
+                val listItems = listOf("day", "week")
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.trending),
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                    MyDropdownMenu(
+                        listItems = listItems,
+                        onItemClick = onItemClick
+                    )
+                }
                 Spacer(Modifier.height(12.dp))
                 LazyRow(
                     contentPadding = PaddingValues(5.dp),
@@ -60,6 +88,46 @@ fun Home(
                             StreamingItemCard(streamingItem = streamingItem)
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyDropdownMenu(
+    listItems: List<String>,
+    onItemClick: (String) -> Unit
+) {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    var isExpanded by remember { mutableStateOf(false) }
+    Box {
+        Surface(
+            shape = RoundedCornerShape(10.dp),
+            color = Color.Blue,
+            modifier = Modifier
+                .size(height = 28.dp, width = 80.dp)
+                .clickable { isExpanded = !isExpanded }
+        ) {
+            Text(
+                text = listItems[selectedIndex],
+                color = Color.White
+            )
+        }
+        DropdownMenu(
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = false }
+        ) {
+            listItems.forEachIndexed { index, s ->
+                if (selectedIndex != index) {
+                    DropdownMenuItem(
+                        text = { Text(s) },
+                        onClick = {
+                            selectedIndex = index
+                            onItemClick(s)
+                            isExpanded = false
+                        }
+                    )
                 }
             }
         }
--- feature/home/src/main/java/com/anshtya/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/anshtya/home/HomeViewModel.kt
@@ -1,15 +1,19 @@
 package com.anshtya.home
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.anshtya.data.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     homeRepository: HomeRepository,
+    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     /*
     as on tmdb home page
@@ -18,6 +22,18 @@ class HomeViewModel @Inject constructor(
     3. search
     */
 
-    val trendingMovies = homeRepository.getTrendingMovies("day")
-        .cachedIn(viewModelScope)
+    private val _trendingTimeWindow = savedStateHandle
+        .getStateFlow(TRENDING_TIME_WINDOW, "day")
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val trendingMovies =_trendingTimeWindow.flatMapLatest {
+        homeRepository.getTrendingMovies(it)
+            .cachedIn(viewModelScope)
+    }
+
+    fun setTrendingTimeWindow(timeWindow: String) {
+        savedStateHandle[TRENDING_TIME_WINDOW] = timeWindow
+    }
+
 }
+const val TRENDING_TIME_WINDOW = "trendingTimeWindow"

5d1bee3e33ec095c8746913c330a5099c77eca81
Ansh Tyagi <tyagiansh486@gmail.com>
2023-12-24 08:31:54 +0530

    move FilterDropdownMenu to core/ui

---
+++ core/ui/src/main/java/com/anshtya/ui/DropdownMenu.kt
@@ -0,0 +1,68 @@
+package com.anshtya.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FilterDropdownMenu(
+    filters: List<Int>,
+    selectedFilterIndex: Int,
+    onFilterClick: (Int) -> Unit
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val selectedOption = filters[selectedFilterIndex]
+    Box {
+        Surface(
+            shape = RoundedCornerShape(10.dp),
+            color = MaterialTheme.colorScheme.primaryContainer,
+            modifier = Modifier
+                .size(height = 28.dp, width = 100.dp)
+                .clickable { isExpanded = !isExpanded }
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = stringResource(id = selectedOption))
+            }
+        }
+        DropdownMenu(
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = !isExpanded }
+        ) {
+            filters.forEachIndexed { index, filter ->
+                if (index != selectedFilterIndex) {
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = stringResource(id = filter))
+                        },
+                        onClick = {
+                            onFilterClick(index)
+                            isExpanded = !isExpanded
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
--- feature/home/src/main/java/com/anshtya/home/HomeScreen.kt
+++ feature/home/src/main/java/com/anshtya/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.anshtya.home
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,24 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -42,6 +34,7 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import com.anshtya.data.model.FreeItem
 import com.anshtya.data.model.PopularItem
 import com.anshtya.data.model.TrendingItem
+import com.anshtya.ui.FilterDropdownMenu
 import com.anshtya.ui.StreamingItemCard
 
 private val horizontalPadding = 10.dp
@@ -304,50 +297,5 @@ fun ContentSection(
             selectedFilterIndex = selectedFilterIndex,
             onFilterClick = onFilterClick
         )
-    }
-}
-
-@Composable
-fun FilterDropdownMenu(
-    filters: List<Int>,
-    selectedFilterIndex: Int,
-    onFilterClick: (Int) -> Unit
-) {
-    var isExpanded by remember { mutableStateOf(false) }
-    val selectedOption = filters[selectedFilterIndex]
-    Box {
-        Surface(
-            shape = RoundedCornerShape(10.dp),
-            color = MaterialTheme.colorScheme.primaryContainer,
-            modifier = Modifier
-                .size(height = 28.dp, width = 100.dp)
-                .clickable { isExpanded = !isExpanded }
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = stringResource(id = selectedOption))
-            }
-        }
-        DropdownMenu(
-            expanded = isExpanded,
-            onDismissRequest = { isExpanded = !isExpanded }
-        ) {
-            filters.forEachIndexed { index, filter ->
-                if (index != selectedFilterIndex) {
-                    DropdownMenuItem(
-                        text = {
-                            Text(text = stringResource(id = filter))
-                        },
-                        onClick = {
-                            onFilterClick(index)
-                            isExpanded = !isExpanded
-                        }
-                    )
-                }
-            }
-        }
     }
 }

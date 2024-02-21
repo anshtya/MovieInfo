f9d7c26429a42653e449b0c9d09fc02470212571
Ansh Tyagi <tyagiansh486@gmail.com>
2024-02-21 19:07:13 +0530

    remove unnecessary unit and instrumentation tests

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -13,8 +13,6 @@ dependencies {
     implementation(projects.core.network)
 
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 
     implementation(libs.androidx.paging.runtime)
 }

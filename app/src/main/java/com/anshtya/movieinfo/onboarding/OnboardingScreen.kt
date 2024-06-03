aaa683c8ad28de33fd4c6d9a12437ad946dd8374
Ansh Tyagi <tyagiansh486@gmail.com>
2024-06-03 13:23:08 +0530

    change content color to default of "Get Started" button

--- app/src/main/java/com/anshtya/movieinfo/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/anshtya/movieinfo/onboarding/OnboardingScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,7 +26,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -99,7 +97,6 @@ fun OnboardingScreen(
                 ) {
                     Button(
                         onClick = navigateToAuth,
-                        colors = ButtonDefaults.buttonColors(contentColor = Color.Black),
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(56.dp)

766f7bba490226a35c76f76ae608041cad3607ad
Ansh Tyagi <tyagiansh486@gmail.com>
2024-06-06 16:28:17 +0530

    fix package name

--- app/src/androidTest/java/com/anshtya/movieinfo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/anshtya/movieinfo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.anshtya.movieinfo", appContext.packageName)
+        assertEquals("com.anshtya.movieinfo.debug", appContext.packageName)
     }
 }

edc30f033470968fcbe50697090d62206de22ba8
Ansh Tyagi <tyagiansh486@gmail.com>
2024-08-20 22:04:45 +0530

    Enable Compose Compiler metrics

--- build-logic/convention/src/main/kotlin/com/anshtya/movieinfo/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/anshtya/movieinfo/AndroidCompose.kt
@@ -2,7 +2,10 @@ package com.anshtya.movieinfo
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 internal fun Project.configureAndroidCompose(
     commonExtension: CommonExtension<*,*,*,*,*,*>
@@ -22,5 +25,10 @@ internal fun Project.configureAndroidCompose(
 
             add("androidTestImplementation", platform(bom))
         }
+    }
+
+    extensions.configure<ComposeCompilerGradlePluginExtension> {
+        reportsDestination = layout.buildDirectory.dir("compose_compiler")
+        metricsDestination = layout.buildDirectory.dir("compose_compiler")
     }
 }

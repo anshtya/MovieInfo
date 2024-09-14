dcde8fbc8f12d5eca883cd0abce9feb89ffcc2d5
Ansh Tyagi <tyagiansh486@gmail.com>
2024-09-14 22:10:00 +0530

    Remove test and androidTest sourceSet

--- sync/build.gradle.kts
+++ sync/build.gradle.kts
@@ -5,20 +5,12 @@ plugins {
 
 android {
     namespace = "com.anshtya.movieinfo.sync"
-
-    defaultConfig {
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
 }
 
 dependencies {
     implementation(projects.data)
 
-    testImplementation(projects.core.testing)
-    androidTestImplementation(projects.core.testing)
-
     api(libs.androidx.hilt.work)
     api(libs.androidx.work.runtime.ktx)
-
     ksp(libs.androidx.hilt.compiler)
 }
